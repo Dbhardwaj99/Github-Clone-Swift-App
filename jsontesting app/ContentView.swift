e483c11f511ec38070f8a8aaf8a1297af5c22ac6
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2023-12-12 23:03:09 +0530

    Initial Commit

---
+++ jsontesting app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jsontesting app
+//
+//  Created by Divyansh Bhardwaj on 12/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jsontesting app/jsontesting_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  jsontesting_appApp.swift
+//  jsontesting app
+//
+//  Created by Divyansh Bhardwaj on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct jsontesting_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
