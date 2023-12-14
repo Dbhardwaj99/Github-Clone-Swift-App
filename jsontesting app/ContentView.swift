5d587a374fbdc25d9ea4ea2698792f05097ccb43
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2023-12-14 12:48:09 +0530

    Initial app working

--- jsontesting app/ContentView.swift
+++ jsontesting app/ContentView.swift
@@ -8,17 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var user: githubUser?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:20) {
+            AsyncImage(url: URL(string: user?.avatarUrl ?? "")){ 
+                image in image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            }placeholder: {
+                Circle()
+                    .foregroundColor(.secondary)
+                    
+            }.frame(width: 200, height: 200)
+            
+            Text(user?.login ?? "User")
+                .bold()
+                .font(.title3)
+            
+            Text(user?.bio ?? "Bio")
+                .padding()
+            
+            Spacer()
+            
+            Text("Hiiiiiiiiii")
+                .padding()
+            
+            Spacer()
         }
         .padding()
+        .task {
+            do{
+                user = try await getUser()
+            } catch GHError.invalidURL{
+                print("Invalid URL")
+            } catch GHError.invalidData{
+                print("Invalid Data")
+            } catch GHError.invalidResponse{
+                print("Invalid Response")
+            } catch{
+                print("Unexpected Error")
+            }
+        }
+    }
+    
+    func getUser() async throws -> githubUser{
+        let endpoint = "https://api.github.com/users/dbhardwaj99"
+        guard let url = URL(string: endpoint) else {
+            throw GHError.invalidURL}
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            throw GHError.invalidResponse
+        }
+        
+        do{
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(githubUser.self, from: data)
+        }catch{
+            throw GHError.invalidData
+        }
+        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct githubUser: Codable{
+    let login : String
+    let avatarUrl : String
+    let bio : String
+}
+
+enum GHError: Error{
+    case invalidURL
+    case invalidResponse
+    case invalidData
 }
